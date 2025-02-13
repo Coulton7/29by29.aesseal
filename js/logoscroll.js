f15dc1467ec486735726431abcb35c26a00d5ca6
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-02-13 16:54:11 +0000

    REmoving the shadow section referal

--- js/logoscroll.js
+++ js/logoscroll.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function() {
   const textLogo = document.querySelector('.textlogo');
-  const sectionShadow = document.querySelector('.section-shadow');
   const enquiry = document.querySelector('.enq-icon');
   const navbar = document.querySelector('.bg-transparent .navbar-collapse');
   var navLinks = document.querySelectorAll('.navbar-nav a.nav-link');
@@ -10,7 +9,6 @@ document.addEventListener("DOMContentLoaded", function() {
   function fadeScroll() {
       window.addEventListener('scroll', function(e) {
           var scrollPos = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-          sectionShadow.style.opacity = Math.max(0, Math.min(1, +scrollPos / 600));
           textLogo.style.opacity = Math.max(0, Math.min(1, +scrollPos / 600));
           enquiry.style.opacity = Math.max(0, Math.min(1, +scrollPos / 600));
           navbar.style.backgroundColor = "rgba(255,255,255,"+ Math.max(0, Math.min(1, +scrollPos / 600)); +")";
@@ -34,14 +32,12 @@ document.addEventListener("DOMContentLoaded", function() {
   };
 
   if(window.innerWidth > 992) {
-      sectionShadow.style.opacity = -1.03;
       textLogo.style.opacity = -1.03;
       enquiry.style.opacity = -1.03;
       navbar.style.backgroundColor = "rgba(255,255,255,0)";
       fadeScroll();
 
   } else {
-      sectionShadow.style.opacity = 1;
       textLogo.style.opacity = 1;
       enquiry.style.opacity = 1;
       navbar.style.backgroundColor = "rgba(255,255,255,1)";
@@ -50,14 +46,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
   window.addEventListener('resize', function(){
       if(window.innerWidth > 992) {
-          sectionShadow.style.opacity = -1.03;
           textLogo.style.opacity = -1.03;
           enquiry.style.opacity = -1.03;
           navbar.style.backgroundColor = "rgba(255,255,255,0)";
           fadeScroll();
       
       } else {
-          sectionShadow.style.opacity = 1;
           textLogo.style.opacity = 1;
           enquiry.style.opacity = 1;
           navbar.style.backgroundColor = "rgba(255,255,255,1)";
