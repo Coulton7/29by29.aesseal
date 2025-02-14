3e3706d6ac5dc9db96e85084b32ea94118cb2754
Coulton7 <32391711+Coulton7@users.noreply.github.com>
2025-02-14 12:35:48 +0000

    Small adjustment to the offset

--- js/block-reveal.js
+++ js/block-reveal.js
@@ -1,14 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   var recentVid = document.querySelector('.recent-vid');
-  var offset = 250;
+  var offset = 450;
   var recentVidPosition = recentVid.getBoundingClientRect().top;
   var offsetPostion = recentVidPosition + window.scrollY - offset;
 
   function getUrlVars(){
     var option = [], tag;
     var tags = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-    console.log(tags);
     for(var i = 0; i < tags.length; i++)
     {
       tag = tags[i].split('=');
